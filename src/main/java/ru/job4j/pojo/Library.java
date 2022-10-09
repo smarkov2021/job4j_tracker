8eec934d3921d345b02dff1e8f7061fb171e0546
smarkov <markovsv777@mail.ru>
2022-10-09 15:46:26 +0300

    Коммит Library

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book adventure = new Book("Adventure", 118);
+        Book fall = new Book("Fall of Bastile", 323);
+        Book wolf = new Book("Lonely wolf", 459);
+        Book cleancode = new Book("Clean Code", 1495);
+
+        Book[] books = new Book[4];
+        books[0] = adventure;
+        books[1] = wolf;
+        books[2] = fall;
+        books[3] = cleancode;
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPagesCount());
+        }
+
+        Book first = books[0];
+        books[0] = books[3];
+        books[3] = first;
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getName() == "Clean Code") {
+                System.out.println(bk.getName() + " - " + bk.getPagesCount());
+            }
+        }
+    }
+}
