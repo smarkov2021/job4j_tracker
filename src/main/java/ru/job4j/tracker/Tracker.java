f8008bc00b303481ce18a78e2403b0fd4d326106
smarkov <markovsv777@mail.ru>
2022-10-11 12:34:40 +0300

    Коммит Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,7 +14,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[100];
+        Item[] rsl = new Item[size];
         int count = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
