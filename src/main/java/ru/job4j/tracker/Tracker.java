405dd5d0692820c33092286e605c99693ec6ae22
smarkov <markovsv777@mail.ru>
2022-10-11 09:08:18 +0300

    Коммит Items

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,24 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] rsl = new Item[100];
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null && item.getName().equals(key)) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        return items;
     }
 
     public Item findById(int id) {
