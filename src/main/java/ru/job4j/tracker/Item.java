3738d89fb8108835add83bbcab22255437d0ced4
smarkov <markovsv777@mail.ru>
2022-10-12 13:21:29 +0300

    Коммит Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,10 +3,13 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
 
     public Item() {
     }
@@ -18,6 +21,15 @@ public class Item {
     public Item(int id, String name) {
         this.name = name;
         this.id  =  id;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created.format(FORMATTER) +
+                '}';
     }
 
     public int getId() {
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item first = new Item(1, "Первый");
+        System.out.println(first);
+    }
+}
