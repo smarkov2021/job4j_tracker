b8d8f29430d7dc34fbc940afe8fbcc589bd31dc7
smarkov <markovsv777@mail.ru>
2022-10-13 14:33:52 +0300

    Коммит Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,7 +12,7 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-            while (count - matches < 0 || matches < 0 || matches > 3) {
+            while (count - matches < 0 || matches < 1 || matches > 3) {
                 System.out.println(" Выбрано некорректное число спичек."
                         + player + " введите число от 1 до 3, не больше оставшегося числа " + count);
                 matches = Integer.parseInt(input.nextLine());
