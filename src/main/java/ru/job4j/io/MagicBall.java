64baf2c4bc7c08492787a5b3a862c4ba77559c75
smarkov <markovsv777@mail.ru>
2022-10-13 14:13:05 +0300

    Коммит MagicBall

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String ask = input.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0:
+                System.out.println("Да");
+            case 1:
+                System.out.println("Нет");
+            default:
+                System.out.println("Может быть");
+        }
+    }
+}
