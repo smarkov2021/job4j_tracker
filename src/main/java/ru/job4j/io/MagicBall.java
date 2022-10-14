0d273192c46e726113ebf5ed0c1c8d44ee645f92
smarkov <markovsv777@mail.ru>
2022-10-14 13:32:30 +0300

    Коммит MagicBall

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -10,12 +10,9 @@ public class MagicBall {
         String ask = input.nextLine();
         int answer = new Random().nextInt(3);
         switch (answer) {
-            case 0:
-                System.out.println("Да");
-            case 1:
-                System.out.println("Нет");
-            default:
-                System.out.println("Может быть");
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
         }
     }
 }
