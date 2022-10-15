6f6b42316e5c1103d7605779617e299ce67f49c9
smarkov <markovsv777@mail.ru>
2022-10-15 18:38:22 +0300

    Коммит Transport

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public double refuel(double value) {
+        return 0;
+    }
+}
