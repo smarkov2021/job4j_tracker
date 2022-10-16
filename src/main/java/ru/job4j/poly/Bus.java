9b9f835de37f79b57000ad6b2767d260025864d1
smarkov <markovsv777@mail.ru>
2022-10-16 08:17:55 +0300

    Коммит Transport

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,19 +1,20 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private double price = 50;
 
     @Override
     public void drive() {
-
+        System.out.println("Поездка началась");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println(" Количество пассажиров " + count);
     }
 
     @Override
     public double refuel(double value) {
-        return 0;
+        return value * price;
     }
 }
