04ed735ccc28c8e0214152093d57527028762db9
smarkov <markovsv777@mail.ru>
2022-11-07 19:58:51 +0300

    Коммит PhoneDictionaryTest

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
+
+    @Test
+    public void whenEmptyResult() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Moskow");
+        assertThat(persons.size()).isEqualTo(0);
+    }
 }
