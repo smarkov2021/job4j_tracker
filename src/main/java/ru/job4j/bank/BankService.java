34c04e9fa46cf92f1d41197dd41e8b2ad214902f
smarkov <markovsv777@mail.ru>
2022-12-02 18:47:11 +0300

    Коммит java doc

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,18 +7,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Класс описывает работу структуры, где ключем является пользователь, а значением список связанных с ним счетов.
+ * структура устроена по принципу FIFO (first in - first out)
+ * @author SERGEY MARKOV
+ * @version 1.0
+ */
 public class BankService {
+
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод принимает на вход объект типа user и добавляет его в очередь
+     * при условии, что нет пользователя с таким же значением поля passport
+     * @param user пользователь, которого необходимо добавить в структуру
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод удаляет пользователя с заданным значением поля passport
+     * @param passport паспортные данные пользователя
+     * @return результат удаления пользователя
+     */
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
 
+    /**
+     * Метод добавляет счет для пользователя.
+     * Осуществляется поиск пользователя по паспортным данным.
+     * У найденного пользователя поднимаем список всех счетов, и добавляем новый.
+     * Если пользователь не найден, выходим из метода
+     * @param passport паспортные данные пользователя
+     * @param account счет, который необходимо привязать
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -29,6 +53,12 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод осуществляет поиск пользователя по его паспортным данным.
+     * Если пользователь не найден, то результатом работы метода будет null.
+     * @param passport паспортные данные пользователя.
+     * @return пользователь с указанными паспортными данными.
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
@@ -38,6 +68,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод осуществляет поиск счета пользователя по паспортным данным и реквизитам.
+     * Сначала находим пользователя, и поднимаем  список всех его счетов.
+     * Последовательно перебирая список, находим совпадающий по полю requisite.
+     * В случае, если пользователь не найден результат метода будет null.
+     * @param passport паспортные данные пользователя
+     * @param requisite данные реквизитов
+     * @return возвращает счет пользователя, объект класса Account
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -51,6 +90,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяет перевести сумму с одного счета на другой.
+     * @param srcPassport паспортные данные пользователя отправителя.
+     * @param srcRequisite реквизиты счета списания.
+     * @param destPassport паспортные данные пользователя получателя.
+     * @param destRequisite реквизиты счета получателя.
+     * @param amount сумма перевода.
+     * @return результат выполнения операции
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
@@ -63,6 +111,11 @@ public class BankService {
         return false;
     }
 
+    /**
+     * Вывод списка всех счетов у пользователя
+     * @param user пользователь.
+     * @return список счетов.
+     */
     public List<Account> getAccounts(User user) {
         return users.get(user);
     }
