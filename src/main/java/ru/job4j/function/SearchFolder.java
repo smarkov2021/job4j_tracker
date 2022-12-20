811e9ebd81a9fe6a3015f2fec6e6df7dc93d64a6
smarkov <markovsv777@mail.ru>
2022-12-20 17:47:25 +0300

    Коммит Folder

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -5,18 +5,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class SearchFolder {
-    public static List<Folder> filterSize(List<Folder> list) {
-        List<Folder> rsl = new ArrayList<>();
-        Predicate<Folder> pred = (folder -> folder.getSize() > 100);
-        return filter(rsl, pred);
-    }
-
-    public static List<Folder> filterName(List<Folder> list) {
-        List<Folder> rsl = new ArrayList<>();
-        Predicate<Folder> pred = (folder -> folder.getName().contains("bug"));
-        return filter(rsl, pred);
-    }
-
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         List<Folder> rsl = new ArrayList<>();
         for (Folder f : list) {
