f42f6f60408a26f095f0017704834a8121a61ea8
smarkov <markovsv777@mail.ru>
2022-12-21 20:35:27 +0300

    Коммит FunctionCalculator

--- src/main/java/ru/job4j/function/FunctionCalculator.java
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 public class FunctionCalculator {
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList<>();
-        for (int index = start; index < end ; index++) {
+        for (int index = start; index < end; index++) {
             result.add(func.apply((double) index));
         }
         return result;
