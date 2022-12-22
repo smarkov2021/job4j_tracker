38715114f1e421c7fbb25fb40588a0d2dee1487c
smarkov <markovsv777@mail.ru>
2022-12-22 18:15:49 +0300

    Коммит PhoneDictionary

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,9 +11,11 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine = (person -> person.getName().contains(key) || person.getSurname().contains(key)
-                || person.getPhone().contains(key)
-                || person.getAddress().contains(key));
+        Predicate<Person> checkName = (person ->  person.getName().contains(key));
+        Predicate<Person> checkSurname = (person ->  person.getSurname().contains(key));
+        Predicate<Person> checkPhone = (person ->  person.getPhone().contains(key));
+        Predicate<Person> checkAddress = (person ->  person.getAddress().contains(key));
+        Predicate<Person> combine = checkName.or(checkSurname).or(checkPhone).or(checkAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
