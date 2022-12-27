bfcd2d82ac673e8d7cc4b1656f2ed262d2183184
smarkov <markovsv777@mail.ru>
2022-12-27 19:44:00 +0300

    Коммит Profiles

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,4 +8,15 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
     }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        Comparator<Address> comparator = (left, right) -> {
+           return left.getCity().compareTo(right.getCity());
+        };
+        return profiles.stream().map(profile -> profile.getAddress())
+                .sorted(comparator)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
 }
