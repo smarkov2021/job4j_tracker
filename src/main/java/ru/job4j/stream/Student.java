6428a954da883cda58b27cc797bd810c1ba5ec74
smarkov <markovsv777@mail.ru>
2022-12-27 18:15:39 +0300

    Коммит Student

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -29,8 +29,7 @@ public class Student {
             return false;
         }
         Student student = (Student) o;
-        return score == student.score &&
-                Objects.equals(surname, student.surname);
+        return score == student.score && Objects.equals(surname, student.surname);
     }
 
     @Override
