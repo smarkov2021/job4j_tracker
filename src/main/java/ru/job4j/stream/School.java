20d9bb05b32be7c2c6cc3fabeebddbc429c60872
smarkov <markovsv777@mail.ru>
2022-12-27 18:16:19 +0300

    Коммит Student

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
-                .filter(student -> predict.test(student))
+                .filter(student -> predict.test(student))v
                 .collect(Collectors.toList());
     }
 }
