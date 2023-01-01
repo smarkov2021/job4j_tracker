8335eb9843ca768badcdbdf3d1a573c6c38dcf56
smarkov <markovsv777@mail.ru>
2023-01-01 16:48:10 +0300

    Коммит School

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
-                .filter(student -> predict.test(student))
+                .filter(predict)
                 .collect(Collectors.toList());
     }
 }
