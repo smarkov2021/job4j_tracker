6dcb79750d6886be44d72a9c720e6a6a67578fa0
smarkov <markovsv777@mail.ru>
2023-01-05 09:19:19 +0300

    Коммит MatrixToList

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -9,7 +9,7 @@ public class MatrixToList {
 
     public static List<Integer> convert(Integer[][] matrix) {
         return Stream.of(matrix)
-                .flatMap(e -> Arrays.stream(e))
+                .flatMap(Arrays::stream)
                 .collect(Collectors.toList());
     }
 }
